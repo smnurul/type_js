e0426fbaa2fef5f3d8ff366b537728039c10edc8
SM NURUL ISLAM <43316959+smnurul@users.noreply.github.com>
2018-09-21 22:56:48 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($){
+    
+var typed = new Typed('.test_typed', {
+  strings: ["Nurul Islam.", "Designer.", "Developer.", "Freelancer.", "Photographer","Funny_leader"],
+  typeSpeed: 100,
+    loop:true
+});
+
+    
+});
+
+
